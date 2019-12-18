d09f4290d9c6c0c7bb7cfafa5d7a5f05e776ea63
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-18 23:47:51 +0700

    Dev-AddCommonLibraries: Add libraries for navigation - setup more for Android

--- android/app/src/main/java/com/rntinderclone/MainActivity.java
+++ android/app/src/main/java/com/rntinderclone/MainActivity.java
@@ -2,14 +2,32 @@ package com.rntinderclone;
 
 import com.facebook.react.ReactActivity;
 
+// React Native Gesture Handler
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "RNTinderClone";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "RNTinderClone";
+    }
+
+    /**
+     * React Native Gesture Handler
+     * */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
 }
