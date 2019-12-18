f739f8bda7edb5bbdafc8cb64274ca6c729f905d
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-18 23:56:13 +0700

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': 0,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 };
