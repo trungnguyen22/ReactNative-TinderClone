62c994038f127976ac1501d9e7b653ebd09cb582
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-19 12:18:36 +0700

    Update HomeScreen, demo CardSwiper

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+
+import {commonStyles} from '../../theme';
+import CardSwiper from '../../components/CardSwiper';
+import Card from '../../components/Card';
+
+// demo purposes only
+function* range(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [...range(1, 50)],
+      swipedAllCards: false,
+      swipeDirection: '',
+      cardIndex: 0,
+    };
+  }
+
+  renderCard = (card, index) => {
+    return <Card />;
+  };
+
+  onSwiped = type => {
+    console.log(`on swiped ${type}`);
+  };
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={commonStyles.container}>
+        <CardSwiper
+          dataSource={this.state.cards}
+          cardIndex={this.state.cardIndex}
+          renderCard={this.renderCard}
+          onSwiped={this.onSwiped}
+        />
       </View>
     );
   }
