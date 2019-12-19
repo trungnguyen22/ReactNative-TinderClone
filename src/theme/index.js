ce1c9b30d5e76c7cc42c4550b159f958a9e237b3
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-19 12:18:52 +0700

    Update theme index

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,4 +1,5 @@
 import {commonStyles} from './Styles';
 import {AppColorPallete} from './Colors';
+import {SCREEN_WIDTH, SCREEN_HEIGHT} from './Dimensions';
 
-export {commonStyles, AppColorPallete};
+export {commonStyles, AppColorPallete, SCREEN_WIDTH, SCREEN_HEIGHT};
