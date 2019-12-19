b500509a2b7dfb08bc44f59ee929fbc81dc62738
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-19 07:46:06 +0700

    Update index.js - import Reactotron

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /**
- * @format
+ * @author: Nguyen Quoc Trung - email: johniusnguyen22297@gmail.com
  */
 
+import './src/configs/ReactotronConfig';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
