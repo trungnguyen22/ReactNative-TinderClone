6cff38cd8efa91fddf2a4f2a6000a8f863cac8b9
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-19 07:59:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 /**
- * @author: Nguyen Quoc Trung
+ * @author: Nguyen Quoc Trung - email: johniusnguyen22297@gmail.com
  */
 
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store';
+
 import MainScreen from './screens/Root/MainScreen';
 import {commonStyles} from './theme';
 
 class App extends Component {
   render() {
     return (
-      <View style={commonStyles.container}>
-        <MainScreen />
-      </View>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <View style={commonStyles.container}>
+            <MainScreen />
+          </View>
+        </PersistGate>
+      </Provider>
     );
   }
 }
