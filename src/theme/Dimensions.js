934e0e6360b50531c6cc3c4b7d67ef93b2273940
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-19 12:18:43 +0700

    Create Dimensions.js

---
+++ src/theme/Dimensions.js
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+export const dimensions = Dimensions.get('window');
+
+export const SCREEN_WIDTH = dimensions.width;
+export const SCREEN_HEIGHT = dimensions.height;
