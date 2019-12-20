d51b89e282e5481480ee2736b53fd29008f410ca
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-21 00:33:58 +0700

    Update randomPerson reducer

--- src/redux/reducers/randomPerson.js
+++ src/redux/reducers/randomPerson.js
@@ -3,11 +3,18 @@ import {
   IS_GETTING_RANDOM_PERSON,
   UPDATE_LIST_FAVORITE_PERSON,
   CLEAR_LIST_FAVORITE_PERSON,
+  DELETE_RANDOM_PERSON,
 } from '../actions/types/RandomPersonTypes';
+import _ from 'lodash';
 
 export function listRandomPerson(state = [], action) {
-  if (action.type === UPDATE_LIST_RANDOM_PERSON) {
-    return [...state, action.payload];
+  switch (action.type) {
+    case UPDATE_LIST_RANDOM_PERSON:
+      return [...state, action.payload];
+    case DELETE_RANDOM_PERSON:
+      return state.filter(
+        person => !_.isEqual(person.phone, action.payload.phone),
+      );
   }
   return state;
 }
