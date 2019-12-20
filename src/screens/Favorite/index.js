52dfcfc63ea317a4e9803b0ccf1d7fd423fa6517
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-20 11:57:22 +0700

    Update FavoriteScreen

--- src/screens/Favorite/index.js
+++ src/screens/Favorite/index.js
@@ -1,14 +1,76 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {connect} from 'react-redux';
+import {Text, View, StyleSheet} from 'react-native';
+import {commonStyles} from '../../theme';
+import Card from '../../components/Card';
+import CircleImage from '../../components/CircleImage';
+import {FlatList} from 'react-native-gesture-handler';
+import {scale} from 'react-native-size-matters';
+
+const styles = StyleSheet.create({
+  favItemContainer: {
+    backgroundColor: 'white',
+    marginBottom: 8,
+  },
+  favItem: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  favTitle: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  favDesc: {
+    color: '#c4c4c4',
+    fontSize: 14,
+  },
+});
+
+const FavoriteItem = ({avatarURL, title, description}) => (
+  <Card
+    containerStyle={styles.favItemContainer}
+    child={
+      <View style={styles.favItem}>
+        <CircleImage imageSize={scale(44)} imageSource={{uri: avatarURL}} />
+        <View style={{marginLeft: 16}}>
+          <Text style={styles.favTitle}>{title}</Text>
+          <Text style={[styles.favDesc, {marginTop: 4}]}>{description}</Text>
+        </View>
+      </View>
+    }
+  />
+);
 
 class FavoritesScreen extends Component {
+  renderFavoriteItem = ({item, index}) => (
+    <FavoriteItem
+      avatarURL={item.avatarURL}
+      title={item.fullName}
+      description={`${item.address} - ${item.phone}`}
+    />
+  );
+
+  renderFavoriteList = listFavoritePerson => (
+    <FlatList
+      data={listFavoritePerson}
+      renderItem={this.renderFavoriteItem}
+      keyExtractor={item => item.fullName}
+    />
+  );
+
   render() {
+    const {listFavoritePerson} = this.props;
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={[commonStyles.container, {padding: 8}]}>
+        {this.renderFavoriteList(listFavoritePerson)}
       </View>
     );
   }
 }
 
-export default FavoritesScreen;
+const mapStateToProps = state => ({
+  listFavoritePerson: state.listFavoritePerson,
+});
+
+export default connect(mapStateToProps, null)(FavoritesScreen);
