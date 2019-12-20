1d7a35581525656dec69a64a00d718e9842ed3d2
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-20 11:56:28 +0700

    Update reducer index

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,2 +1,7 @@
 export {todos} from './todos';
 export {visibilityFilter} from './visibilityFilter';
+export {
+  listRandomPerson,
+  listFavoritePerson,
+  isGettingRandomPerson,
+} from './randomPerson';
