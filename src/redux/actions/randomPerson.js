9a4af50a142de192355f458fb9fe8a8a2dcb6670
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-21 00:33:42 +0700

    Update randomPerson.js

--- src/redux/actions/randomPerson.js
+++ src/redux/actions/randomPerson.js
@@ -3,6 +3,7 @@ import {
   UPDATE_LIST_RANDOM_PERSON,
   UPDATE_LIST_FAVORITE_PERSON,
   CLEAR_LIST_FAVORITE_PERSON,
+  DELETE_RANDOM_PERSON,
 } from './types/RandomPersonTypes';
 import AppClient from '../../networkings/AppClient';
 import _ from 'lodash';
@@ -34,24 +35,38 @@ export function clearListFavoritePerson() {
   };
 }
 
+export function deleteRandomPerson(phone) {
+  return {
+    type: DELETE_RANDOM_PERSON,
+    payload: phone,
+  };
+}
+
 /**
  * Getting a random person and append that person data to list person in store
  */
 export function fetchRandomPerson() {
   const doneFetching = (dispatch, person) => {
-    const updatePerson = {
-      ...person,
-      receivedAt: Date.now(),
-      status: !_.isEmpty(person),
-    };
-    dispatch(updateListRandomPerson(updatePerson));
+    if (person) {
+      const updatePerson = {
+        ...person,
+        receivedAt: Date.now(),
+        status: !_.isEmpty(person),
+      };
+      dispatch(updateListRandomPerson(updatePerson));
+    }
     dispatch(isGettingRandomPerson(false));
   };
 
   return dispatch => {
     dispatch(isGettingRandomPerson(true));
-    return AppClient.getRandomPerson().then(person => {
-      doneFetching(dispatch, person);
-    });
+    return AppClient.getRandomPerson().then(
+      person => {
+        doneFetching(dispatch, person);
+      },
+      error => {
+        doneFetching(dispatch, null);
+      },
+    );
   };
 }
