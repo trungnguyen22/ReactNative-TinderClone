a1f7dcee4e18bc3c907a247b9f8aedb1e4963b9f
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-21 00:33:25 +0700

    Create Empty view

---
+++ src/components/Empty/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginBottom: 16,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  reloadImg: {
+    width: 32,
+    height: 32,
+  },
+});
+
+const EmptyData = ({onReloadPress}) => (
+  <View style={styles.container}>
+    <Image
+      style={styles.image}
+      source={require('../../assets/images/ic_open_box_64.png')}
+    />
+    <Text style={styles.text}>
+      {'Data is not available. There is something wrong.'}
+    </Text>
+    <TouchableOpacity onPress={onReloadPress}>
+      <Image
+        style={styles.reloadImg}
+        source={require('../../assets/images/ic_reload_64.png')}
+      />
+    </TouchableOpacity>
+  </View>
+);
+
+export default EmptyData;
