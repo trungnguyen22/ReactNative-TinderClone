9ee3666ac5531b1cac6c220b5d2d2dbe71ea5a60
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-20 11:56:17 +0700

    Create randomPerson.js

---
+++ src/redux/reducers/randomPerson.js
@@ -0,0 +1,23 @@
+import {
+  UPDATE_LIST_RANDOM_PERSON,
+  IS_GETTING_RANDOM_PERSON,
+  UPDATE_LIST_FAVORITE_PERSON,
+} from '../actions/types/RandomPersonTypes';
+
+export function listRandomPerson(state = [], action) {
+  if (action.type === UPDATE_LIST_RANDOM_PERSON) {
+    return [...state, action.payload];
+  }
+  return state;
+}
+
+export function listFavoritePerson(state = [], action) {
+  if (action.type === UPDATE_LIST_FAVORITE_PERSON) {
+    return [...state, action.payload];
+  }
+  return state;
+}
+
+export function isGettingRandomPerson(state = false, action) {
+  return action.type === IS_GETTING_RANDOM_PERSON ? action.payload : state;
+}
